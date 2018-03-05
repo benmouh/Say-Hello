9358ddb80674acea74728277bb880de14e583c3d
aitbenmouh <lhoussaine.aitbenmouh@wakanda.io>
2018-03-05 09:25:06 +0000

    automated test using mocha

---
+++ test/index.js
@@ -0,0 +1,4 @@
+var hello = require("./build/Release/addon.node");
+console.log(hello.sayHello("Lhoussaine"));
+console.log("Finish");
+require('assert').equal(hello.sayHello("Lhoussaine"),"Hello Lhoussaine");
