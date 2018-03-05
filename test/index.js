9a03c685f873e4551aab2f23a7b6d88a6839fdd9
aitbenmouh <lhoussaine.aitbenmouh@wakanda.io>
2018-03-05 09:39:00 +0000

    automated test using mocha

--- test/index.js
+++ test/index.js
@@ -1,4 +1,12 @@
 var hello = require("./../build/Release/addon.node");
+var assert = require('assert');
+
 console.log(hello.sayHello("Lhoussaine"));
 console.log("Finish");
-require('assert').equal(hello.sayHello("Lhoussaine"),"Hello Lhoussaine");
+assert.equal(hello.sayHello("Lhoussaine"),"Hello Lhoussaine");
+
+describe('.sayHello(param)', function () {
+  it('returns hello the name given in parameter', function () {
+    assert.deepEqual(hello.sayHello("Lhoussaine"), "Hello Lhoussaine");
+  });
+});
